30519d13c9c822d70e3c0c562363e8cb8beb4e1f
TimNurMur <tim.nur-mur@mail.ru>
2022-07-27 10:19:29 +0400

    feat: add code js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  /*const stack = [];
+  for (let i = 0; i < (str.length - 1); i++)  {
+    if (str[i] === ('(' || '[' || '{')) {
+      stack.push(str[i]);
+    } else if (str[i] === (')' || ']' || '}')) {
+      if  ((stack === []) || (stack[i] !== str[i])) {
+        return false;
+      } else {
+        stack.pop();
+      }
+    }
+  }
+  return (stack === []) ? true : false;
+    //for (el of bracketsConfig) {
+    //stack.push(el.join(''));
+    //}
+  */
+  if (str.length % 2 !== 0) {return false;}
+  const stack = [];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === 0) {
+      stack.push(str[i]);
+    } else if (str[i] === Object.fromEntries(bracketsConfig)[stack[stack.length - 1]]) {
+      stack.pop();
+    } else {
+      stack.push(str[i]);
+    }
+  }
+  return stack.length === 0;
 }
